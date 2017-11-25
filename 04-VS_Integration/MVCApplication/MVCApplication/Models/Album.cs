1ee5eba16ab45ba62beed1e29027e91b55ab92b1
Omar Sáenz Mendoza <osaenz@nationalresearch.com>
2017-11-25 11:29:56 -0800

    Visual Studio integration excercises

---
+++ 04-VS_Integration/MVCApplication/MVCApplication/Models/Album.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCApplication.Models
+{
+    public class Album
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Band { get; set; }
+    }
+}
